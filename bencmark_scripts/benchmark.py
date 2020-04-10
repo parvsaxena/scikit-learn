1a129b4cc40d15e9c6ae34c3b99605a0c12acbf6
parvsaxena <saxena.parv@gmail.com>
2020-04-10 01:20:56 -0400

    Adding script to generate plot

--- bencmark_scripts/benchmark.py
+++ bencmark_scripts/benchmark.py
@@ -202,8 +202,8 @@ def benchmark_mnist(n_estimators = 2048, interleave_depth = 2, batch_size = 1, n
 def compiled_trees(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_threads=8):
     method_name = "compiled_trees"
     print("Calling compiled trees")
-    # batches = [batch_size]
-    batches = [1, 10, 100]
+    batches = [1]
+    # batches = [1, 10, 100]
     # batches = [10000, 5000, 1000, 500, 100, 10, 1]
     arr = np.empty(shape=(int)(cache_size/4), dtype=np.float32)  # 4bytes for n.float32
 
@@ -235,7 +235,7 @@ def compiled_trees(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_thre
                 compiled_predictor.predict(X_test[i:(i+1)*batch_size])
                 delta = (datetime.now() - tstart)
                 compiled_trees_lst.append(delta)
-                f.write(str(i) + ',' + str(delta.total_seconds() * 1000) + '\n')
+                f.write(str(delta.total_seconds() * 1000) + '\n')
 
             f.close()
             op_time = reduce(add, compiled_trees_lst)/Y_test.shape[0]
@@ -260,8 +260,8 @@ def compiled_trees(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_thre
 
 def sklearn_naive(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_threads=8):
     method_name = "sklean_naive"
-    # batches = [1]
-    batches = [1, 10, 100]
+    batches = [1]
+    # batches = [1, 10, 100]
     # batches = [10000, 5000, 1000, 500, 100, 10, 1]
     arr = np.empty(shape=(int)(cache_size/4), dtype=np.float32)  # 4bytes for n.float32
 
@@ -289,7 +289,7 @@ def sklearn_naive(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_threa
                 clf.predict(X_test[i:(i+1)*batch_size])
                 delta = (datetime.now() - tstart)
                 sklearn_naive_lst.append(delta)
-                f.write(str(i) + ',' + str(delta.total_seconds() * 1000) + '\n')
+                f.write(str(delta.total_seconds() * 1000) + '\n')
             f.close()
             op_time = reduce(add, sklearn_naive_lst)/Y_test.shape[0]
             print("Naive operation (us) is", op_time)
@@ -301,8 +301,8 @@ def sklearn_naive(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_threa
 def packed_forest(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_threads=8):
     method_name = "packed_forest"
     print("Calling PackedForest")
-    # batches = [batch_size]
-    batches = [1, 10, 100]
+    batches = [batch_size]
+    # batches = [1, 10, 100]
     # batches = [10000, 5000, 1000, 500, 100, 10, 1]
     arr = np.empty(shape=(int)(cache_size/4), dtype=np.float32)  # 4bytes for n.float32
 
@@ -334,7 +334,7 @@ def packed_forest(clf, X_test, Y_test, interleave_depth=2, batch_size=1, n_threa
                 frst.predict(X_test[i:(i+1)*batch_size], n_threads=n_threads)
                 delta = (datetime.now() - tstart)
                 packed_forest_lst.append(delta)
-                f.write(str(i) + ',' + str(delta.total_seconds()*1000) + '\n')
+                f.write(str(delta.total_seconds()*1000) + '\n')
 
             f.close()
             op_time = reduce(add, packed_forest_lst)/Y_test.shape[0]
---
+++ bencmark_scripts/zaligned_py3.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import matplotlib
+import sys
+import matplotlib.pyplot as plt
+
+##### VIOLIN PLOT #########
+# python3 zaligned.py
+
+
+######GLOBALS########
+plt.rcParams.update({'font.size': 28})
+path1 = 'packed_forest_0_1.csv'
+path2 = 'compiled_trees_0_1.csv'
+path3 = 'sklean_naive_0_1.csv'
+scale = 1.85
+
+
+b0 = np.genfromtxt(path1, delimiter='\n')
+b1 = np.genfromtxt(path2, delimiter='\n')
+b2 = np.genfromtxt(path3, delimiter='\n')
+
+b0 = b0[~np.isnan(b0)]
+b1 = b1[~np.isnan(b1)]
+b2 = b2[~np.isnan(b2)]
+
+box_plot_data = [b0, b1, b2]
+
+data = pd.DataFrame({'PF':box_plot_data[0],
+                     'CT':box_plot_data[1],
+                     'SK-n':box_plot_data[1]
+                     })
+
+sns.set(font_scale=scale)
+sns.set(style="whitegrid")
+
+ax = sns.violinplot(data=data , split=True, inner = "box" )
+    
+ax.set(xlabel='Layout', ylabel='Inference Time in (ms)')
+plt.tight_layout()
+plt.savefig('Layout_vs_ioblocks_'+'.png', dpi=900);
+plt.show()
